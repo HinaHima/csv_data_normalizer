0498f42ddd4a757b34283f3eb4a2eb9c91e1ec67
HinaHima <blankov.boris@mail.ru>
2025-04-03 01:23:59 +0800

    .

--- isCSV.zig
+++ isCSV.zig
@@ -20,23 +20,38 @@ fn getRows(csvData: []const u8) ![][]const u8 {
     return rows.toOwnedSlice();
 }
 
+fn isCellphoneColumn(
+    columnName: []const u8,
+) !bool {
+    const name = [_]u8{ 112, 104, 111, 110, 101 };
+
+    for (columnName, 0..) |symb, i| {
+        if (symb == name[i]) {
+            continue;
+        } else {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 fn findCellphoneColumn(rows: [][]const u8) !bool {
-    var columnNumber: u8 = 0;
+    var columnNumber: usize = 0;
 
     for (rows, 0..) |value, i| {
         if (i == 0) {
-            const columnName = [_]u8{ 112, 104, 111, 110, 101 };
-            const startIndex = 0;
-
-            if (value[startIndex .. startIndex + 5] == columnName) {
-                std.debug.print("{any}", .{"true"});
-            }
+            var startIndex: usize = 0;
 
             for (value, 0..) |symb, si| {
                 if (symb == 44) {
                     columnNumber += 1;
                     startIndex = si + 1;
-                } else {}
+                } else if (try isCellphoneColumn(value[startIndex .. startIndex + 5])) {
+                    columnNumber = si + 1;
+                    std.debug.print("{any}", .{columnNumber});
+                    break;
+                }
             }
         }
     }
